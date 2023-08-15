6473d1b82336f05775b57b87173ee9c0b7fad76e
etelight <etelight@gmail.com>
2023-08-15 17:02:00 +0300

    all

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,53 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Notification from './Notification/Notification';
+import Section from './Section/Section';
+import Statistics from './Statistics/Statistics';
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    total: 0,
+  };
+
+  handleClick = key => {
+    this.setState(prevState => {
+      return {
+        [key]: prevState[key] + 1,
+        total: prevState.total + 1,
+      };
+    });
+  };
+
+  calcPositivePercentage = () => {
+    const { good, total } = this.state;
+    return total === 0 ? 0 : Math.round((good / total) * 100);
+  };
+
+  render() {
+    const options = ['good', 'neutral', 'bad'];
+    const { good, neutral, bad } = this.state;
+    return (
+      <>
+        <Section title={'Please, leave feedback'}>
+          <FeedbackOptions clickFeedback={this.handleClick} options={options} />
+        </Section>
+        <Section title={'Statistics'}>
+          {this.state.total === 0 ? (
+            <Notification messege={'There is no feedback'} />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.state.total}
+              positivePercentage={this.calcPositivePercentage()}
+            />
+          )}
+        </Section>
+      </>
+    );
+  }
+}
